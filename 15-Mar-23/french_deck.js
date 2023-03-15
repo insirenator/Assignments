641505ca82a144c2a1b00a29c67bdd1c4392c523
insirenator <arsshakeeb149@protonmail.com>
2023-03-15 17:51:17 +0530

    Fixed french_deck.js

--- 15-Mar-23/french_deck.js
+++ 15-Mar-23/french_deck.js
@@ -1,7 +1,7 @@
 // Create a French Deck
 const cards = [];
 
-for (let suit in ['H', 'C', 'D', 'S']) { // hearts, clubs, diamonds, spades
+for (let suit of ['H', 'C', 'D', 'S']) { // hearts, clubs, diamonds, spades
 	for (let value = 1; value <= 13; value++)
 		cards.push({suit, value});
 }
@@ -13,15 +13,12 @@ function cardToString(c) {
 	suits = {'H': '\u2665', 'C': '\u2663', 'D': '\u2666', 'S': '\u2660'};
 	values = {1: 'A', 11: 'J', 12: 'Q', 13: 'K'};
 
-	for (let i = 1; i <=10; i++)
+	for (let i = 2; i <=10; i++)
 		values[i] = i;
 
 	return values[c.value] + suits[c.suit];
 }
 
-let c = {
-	suit: 'H',
-	value: 5,
-};
+// console.table(cards);
 
-console.log(cardToString(c));
+console.log(cards.map(cardToString));
